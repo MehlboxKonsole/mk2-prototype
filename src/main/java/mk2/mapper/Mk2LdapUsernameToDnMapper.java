855a61caa949f2c37ce46f131a1e8bdce2a18e5b
Holger Steinhauer <holger+github@willige-entwickler.de>
2016-05-07 20:40:07 +0100

    Suppresses warnings and explains why in

--- src/main/java/mk2/mapper/Mk2LdapUsernameToDnMapper.java
+++ src/main/java/mk2/mapper/Mk2LdapUsernameToDnMapper.java
@@ -4,14 +4,19 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.ldap.core.DistinguishedName;
 import org.springframework.security.ldap.LdapUsernameToDnMapper;
 
+/**
+ * Spring LDAP uses classes implementing LdapUsernameToDnMapper for user name
+ * to User DN mappings. Sadly, the current version is still using the
+ * deprecated DistinguishedName class. To avoid visual noise, I switch off
+ * the warning via @SuppressWarnings
+ */
+@SuppressWarnings("deprecations")
 @ConfigurationProperties(prefix = "mk2.ldap.userDnMapper")
 public class Mk2LdapUsernameToDnMapper implements LdapUsernameToDnMapper {
 
 	private String usernamePattern = "%s@e-mehlbox.eu";
 	private String userAttribute = "cn";
 	private String userBaseDn = "ou=internal,ou=Users";
-
-
 
 	@Override
 	public DistinguishedName buildDn(String username) {
