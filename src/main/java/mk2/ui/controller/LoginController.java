3af505e8149b99d4c314201feee51ac6d79705a1
Holger Steinhauer <holger+github@willige-entwickler.de>
2016-05-09 20:22:05 +0100

    With this refactoring, the actions `changePassword` and `showAddresses` are within the REST entity `user`. This is reflected in the code by re- moving the two separate controllers `EmailAddressController` and `PasswordController` and adding `UserController` instead.
    Last but not least the new structure needs smaller adjustments in the
    html files, so the functionality is still available. This is also added
    with this commit.

--- src/main/java/mk2/ui/controller/LoginController.java
+++ src/main/java/mk2/ui/controller/LoginController.java
@@ -2,11 +2,12 @@ package mk2.ui.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class LoginController {
 
-	@RequestMapping("/login")
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
 	public String login() {
 		return "login";
 	}
