f0eaee0f2e9e0f6a3f282798276bf68a6c297af3
Holger Steinhauer <holger+github@willige-entwickler.de>
2016-06-02 07:47:25 +0100

    Removes the need (and the actual class) for Mk2UserAttributesMapper by introducing ODM attributes in the Mk2User bean. Additionally a copy of application.properties is now available for tests as well.

--- src/main/java/mk2/service/Mk2LdapUserService.java
+++ src/main/java/mk2/service/Mk2LdapUserService.java
@@ -1,17 +1,17 @@
 package mk2.service;
 
-import mk2.mapper.Mk2UserAttributesMapper;
 import mk2.model.Mk2User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ldap.core.ContextSource;
 import org.springframework.ldap.core.LdapTemplate;
+import org.springframework.ldap.support.LdapNameBuilder;
 import org.springframework.security.ldap.LdapUtils;
-import org.springframework.security.ldap.userdetails.LdapUserDetailsManager;
 import org.springframework.stereotype.Service;
 
 import javax.naming.NamingException;
+import javax.naming.ldap.LdapName;
 
 @Service
 public class Mk2LdapUserService {
@@ -32,6 +32,9 @@ public class Mk2LdapUserService {
 			log.info("Given user DN [{}] seems to be invalid.", dn);
 		}
 
-		return ldapTemplate.lookup(relativeName, new Mk2UserAttributesMapper());
+		LdapName rdn = LdapNameBuilder.newInstance(relativeName).build();
+		Mk2User user = ldapTemplate.findByDn(rdn, Mk2User.class);
+
+		return user;
 	}
 }
