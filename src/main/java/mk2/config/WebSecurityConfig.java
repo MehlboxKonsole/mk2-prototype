15dec7f73290fbe40d9dd1a2dac581ff3caaf54a
Holger Steinhauer <holger+github@willige-entwickler.de>
2016-06-07 20:23:48 +0100

    A user with at least one domain assigned is having the role ROLE_DOMAIN_OWNER now. This is dynamically evaluated and checked straight after the actual authentication. Next step: Refactoring.

--- src/main/java/mk2/config/WebSecurityConfig.java
+++ src/main/java/mk2/config/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package mk2.config;
 
+import mk2.auth.Mk2LdapAuthoritiesPopulator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,7 +9,6 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.ldap.userdetails.DefaultLdapAuthoritiesPopulator;
 import org.springframework.security.ldap.userdetails.LdapAuthoritiesPopulator;
 import org.springframework.security.web.header.writers.StaticHeadersWriter;
 
@@ -27,38 +27,38 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http
 				.headers()
-					.addHeaderWriter(new StaticHeadersWriter("Server", "To Serve And Protect"))
-					.addHeaderWriter(new StaticHeadersWriter("X-Content-Type-Options", "nosniff"))
-					.addHeaderWriter(new StaticHeadersWriter("X-Frame-Options", "DENY"));
+				.addHeaderWriter(new StaticHeadersWriter("Server", "To Serve And Protect"))
+				.addHeaderWriter(new StaticHeadersWriter("X-Content-Type-Options", "nosniff"))
+				.addHeaderWriter(new StaticHeadersWriter("X-Frame-Options", "DENY"));
 
 		http
 				.authorizeRequests()
-					.antMatchers("/error", "/static/**", "/webjars/**").permitAll()
-					.anyRequest().authenticated()
-					.and()
+				.antMatchers("/error", "/static/**", "/webjars/**").permitAll()
+				.anyRequest().authenticated()
+				.and()
 				.formLogin()
-					.loginPage("/login")
-					.permitAll()
-					.and()
+				.loginPage("/login")
+				.permitAll()
+				.and()
 				.logout()
-					.permitAll();
+				.permitAll();
 	}
 
 	@Autowired
 	public void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth
 				.ldapAuthentication()
-					.ldapAuthoritiesPopulator(ldapAuthoritiesPopulator())
+				.ldapAuthoritiesPopulator(ldapAuthoritiesPopulator())
 //					.userDnPatterns("cn={0}@e-mehlbox.eu,ou=internal,ou=Users,dc=e-mehlbox,dc=eu")
-					.userDnPatterns("cn={0}@e-mehlbox.eu,ou=internal,ou=Users")
-					.groupSearchBase(LDAP_GROUP_SEARCH_BASE)
-					.groupSearchFilter(LDAP_GROUP_SEARCH_FILTER)
-					.contextSource(contextSource);
+				.userDnPatterns("cn={0}@e-mehlbox.eu,ou=internal,ou=Users")
+				.groupSearchBase(LDAP_GROUP_SEARCH_BASE)
+				.groupSearchFilter(LDAP_GROUP_SEARCH_FILTER)
+				.contextSource(contextSource);
 	}
 
 	@Bean
-	public LdapAuthoritiesPopulator ldapAuthoritiesPopulator () {
-		DefaultLdapAuthoritiesPopulator authoritiesPopulator = new DefaultLdapAuthoritiesPopulator(contextSource, LDAP_GROUP_SEARCH_BASE);
+	public LdapAuthoritiesPopulator ldapAuthoritiesPopulator() {
+		Mk2LdapAuthoritiesPopulator authoritiesPopulator = new Mk2LdapAuthoritiesPopulator(contextSource, LDAP_GROUP_SEARCH_BASE);
 
 		authoritiesPopulator.setDefaultRole("ROLE_USER");
 		authoritiesPopulator.setGroupSearchFilter(LDAP_GROUP_SEARCH_FILTER);
--- src/main/java/mk2/ui/controller/DomainController.java
+++ src/main/java/mk2/ui/controller/DomainController.java
@@ -9,7 +9,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import java.net.IDN;
 import java.util.List;
 
 @Controller
