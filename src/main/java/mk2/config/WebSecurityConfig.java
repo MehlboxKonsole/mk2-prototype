8fbcf673cbdeb9383ebbac205cc0fa6b1eb64d0a
daincredibleholg <holger+github@willige-entwickler.de>
2018-10-29 20:56:16 +0000

    Formatting

--- src/main/java/mk2/config/WebSecurityConfig.java
+++ src/main/java/mk2/config/WebSecurityConfig.java
@@ -32,6 +32,23 @@
  *     along with MehlboxKonsole2.  If not, see <https://www.gnu.org/licenses/>.
  */
 
+/*
+ * This file is part of MehlboxKonsole2.
+ *
+ *     MehlboxKonsole2 is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU Affero General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     MehlboxKonsole2 is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU Affero General Public License for more details.
+ *
+ *     You should have received a copy of the GNU Affero General Public License
+ *     along with MehlboxKonsole2.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package mk2.config;
 
 import mk2.auth.Mk2LdapAuthoritiesPopulator;
@@ -55,8 +72,7 @@ import javax.inject.Inject;
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 
-	final
-	LdapContextSource contextSource;
+	private final LdapContextSource contextSource;
 
 	private static final String LDAP_GROUP_SEARCH_BASE = "ou=Groups";
 	private static final String LDAP_GROUP_SEARCH_FILTER = "uniqueMember={0}";
